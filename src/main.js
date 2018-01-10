307ffb9c277df1589ad9346c67412332a0737542
unknown <shenxj16541@hs.handsome.com.cn>
2018-01-10 13:02:44 +0800

    初始化iview

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import App from './components/App.vue'
+import Routers from './router.js'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(VueRouter)
+Vue.use(iView)
+
+const RouterConfig = {
+  routes: Routers
+}
+const router = new VueRouter(RouterConfig)
 new Vue({
   el: '#app',
   render: h => h(App)
---
+++ src/router.js
@@ -0,0 +1,5 @@
+const routers = [
+
+];
+
+module.exports = routers
